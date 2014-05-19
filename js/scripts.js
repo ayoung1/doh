a01912f04e7cacd04a7302156588e7d81da24b1b
ayoung1 <aaron.young.0015@edmail.edcc.edu>
2014-05-19 02:27:10 -0700

    made it 404-e

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 // My Scripts
 $(document).foundation();
+
+var options = {
+            offset: 50,
+            classes: {
+                clone:   'banner--clone',
+                stick:   'banner--stick',
+                unstick: 'banner--unstick'
+            }
+        };
+
+        // Initialise with options
+        var banner = new Headhesive('.banner', options);
