b14e1fb071e2ad9d72c4a1a7976e97fff06340ab
ayoung1 <aaron.young.0015@edmail.edcc.edu>
2014-05-19 02:29:03 -0700

    404 and js plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 // My Scripts
 $(document).foundation();
 
+var options = {
+            offset: 50,
+            classes: {
+                clone:   'banner--clone',
+                stick:   'banner--stick',
+                unstick: 'banner--unstick'
+            }
+        };
 
-$(function(){
-	var BV = new $.BigVideo();
-	BV.init();
-	BV.show('http://ayoung1.github.io/doh/oceans.mp4');
-
-})
+        // Initialise with options
+        var banner = new Headhesive('.banner', options);
